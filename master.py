da1367d3e86ceabac9f36c4bfcaabc83947ff9cc
Charles Noyes <me@charlien.me>
2015-03-20 22:47:52 -0700

    Update master.py

--- master.py
+++ master.py
@@ -1,2 +1,6 @@
 import tools
 import custom
+# Currently, program execution is done manually through the terminal.
+# This file will eventually become the main cli that the program interfaces with.
+# In order for this to be accomplished, the multithreaded parts of the project
+# need to be refactored.
